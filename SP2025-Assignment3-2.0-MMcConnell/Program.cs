5e42667e6595d7487e19eef19245b869d3cd027f
marykate1 <mkmcconnell1@crimson.ua.edu>
2025-03-17 18:35:04 -0500

    Add project files.

---
+++ SP2025-Assignment3-2.0-MMcConnell/Program.cs
@@ -0,0 +1,16 @@
+namespace SP2025_Assignment3_2._0_MMcConnell
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
+            app.Run();
+        }
+    }
+}
